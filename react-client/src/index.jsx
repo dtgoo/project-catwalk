4fda086f745657e421d4582ce1918bcb4525896e
Tyna Nguyen <ngtyna@gmail.com>
2020-11-12 17:54:26 -0600

    Initialize React App

---
+++ react-client/src/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <h1>Hello World</h1>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
